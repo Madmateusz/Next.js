19a004758d70c6d81bcba93c5c7d3d3435de4d52
Sait_saiter <133907363+Madmateusz@users.noreply.github.com>
2024-12-02 22:47:13 +0100

    Hey

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,10 +1,12 @@
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
-import {useFormStatus} from 'react-dom';
+// import { useActionState } from 'react';
+
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
 
 export default function ShareMealPage() {
-  const status = useFormStatus();
+    // useActionState();
 
   return (
     <>
@@ -45,7 +47,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label='Your image' name='image'/>
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit/>
           </p>
         </form>
       </main>
