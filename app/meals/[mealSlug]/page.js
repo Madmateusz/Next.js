4522111761316ebaad0a87298d4a8a7417b406cc
mateusz <mat3usz.mordziolek@gmail.com>
2024-12-03 11:37:53 +0100

    f

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -5,7 +5,7 @@ import { notFound } from 'next/navigation';
 
 export default function MealDetailsPage({params})
 {
-   FIXME const meal= getMeal(params.MealSlug);
+   const meal= getMeal(params.MealSlug);
 
     if(!meal)
     {
